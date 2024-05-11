95832f97dbe2192bf5e795c13b2d6bde80c9b58d
F0sya <nikitachmyria@gmail.com>
2024-05-11 17:15:07 +0300

    Fifth homework exercise is done.

--- cycles_part2/main.cpp
+++ cycles_part2/main.cpp
@@ -3,10 +3,21 @@ using namespace std;
 
 int main() {
 	int A; cout << "A:"; cin >> A;
+	int temp = A;
+	int sum = 0;
+	for (; temp != 0;) {
+		sum += temp % 10;
+		temp /= 10;
+	}
 
-	for (int B = 1; B < A; B++) {
-		if (A % (B * B) == 0 && A % (B * B * B) != 0) {
-			cout << B << endl;
-		}
+	cout << "Sum: " << sum << endl;
+	cout << "A:" << A << endl;
+
+
+	if ((sum * sum * sum) == (A * A)) {
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << endl;
 	}
 }
