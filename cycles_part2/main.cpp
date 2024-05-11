70dc2433cae1566ca8f484ca3b022960b057ce29
F0sya <nikitachmyria@gmail.com>
2024-05-11 17:19:04 +0300

    Seventh homework exercise is done.

--- cycles_part2/main.cpp
+++ cycles_part2/main.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int main() {
 	int A; cout << "A:"; cin >> A;
+	int B; cout << "B:"; cin >> B;
+	int stop;
+	(A < B) ? stop = A : stop = B;
 
-	for (int B = 1; B < A; B++) {
-		if (A % (B * B) == 0 && A % (B * B * B) != 0) {
-			cout << B << endl;
+	for (int i = 1; i <= stop; i++) {
+		if (A % i == 0 && B % i == 0) {
+			cout << i << " ";
 		}
 	}
 }
